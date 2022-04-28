5d6afea11cee84823ec5fb7d0756dd141b71f705
Ruman-Islam <rumanislam48@gmail.com>
2022-04-29 03:20:19 +0600

    react router installed

---
+++ src/components/Shared/Navbar/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <header>
+            <nav className=''>
+                <div>
+                    <h1>Warehouse</h1>
+                    <input type="text" name="" id="" />
+                    <div>
+                        <ul>
+                            <li>li</li>
+                            <li>li</li>
+                            <li>li</li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Inventory</li>
+                    <li>My Items</li>
+                    <li>Settings</li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Navbar;
